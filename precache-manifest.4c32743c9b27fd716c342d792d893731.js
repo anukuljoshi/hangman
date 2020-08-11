03e04d4f30e16689ee84522a488ceba3e94f759d
Anukul Joshi <anukuljoshi619@gmail.com>
2020-08-11 18:39:43 +0530

    Updates

---
+++ precache-manifest.4c32743c9b27fd716c342d792d893731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9484ff7302796befbab0daf8efae22f4",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "c24924c2aac750478eb7",
+    "url": "/hangman/static/css/main.fae8c264.chunk.css"
+  },
+  {
+    "revision": "973e31a4495480d1933b",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c24924c2aac750478eb7",
+    "url": "/hangman/static/js/main.54431ead.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
